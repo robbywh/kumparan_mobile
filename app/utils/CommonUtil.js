375663fcc17b48efb359b8e448e090bd23e9ffde
Robby Widya Hartono <robby.widyahartono@gmail.com>
2018-03-21 17:18:20 +0700

    adding utils for deviceWidth and deviceHeight

---
+++ app/utils/CommonUtil.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Dimensions } from 'react-native';
+
+export const deviceWidth = Dimensions.get('window').width;
+export const deviceHeight = Dimensions.get('window').height;
