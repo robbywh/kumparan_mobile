e948c07ae66fb3385dd1e0ebda0bb8db3cd5c423
Robby Widya Hartono <robby.widyahartono@gmail.com>
2018-03-22 10:16:21 +0700

    add screen on config

--- app/components/tabBar/navigationConfiguration.js
+++ app/components/tabBar/navigationConfiguration.js
@@ -6,7 +6,42 @@ import Color from 'kumparan_mobile/app/utils/Color'
 
 // VIEWS
 import ListArticlesView from '../tabArticles/views/ListArticlesView';
+
+const navigateOnce = (getStateForAction) => (action, state) => {
+  const {type, routeName} = action;
+  return (
+    state &&
+    type === NavigationActions.NAVIGATE &&
+    routeName === state.routes[state.routes.length - 1].routeName
+  ) ? state : getStateForAction(action, state);
+};
+
+// =================================================================================
+// TAB BOOKS
+// =================================================================================
 import ListBooksView from '../tabBooks/views/ListBooksView';
+import DetailBookView from '../tabBooks/views/DetailBookView';
+
+const NavigatorTabBooks = StackNavigator({
+  DetailBookView: {
+    screen: DetailBookView,
+    navigationOptions: {
+      tabBarVisible:false
+    }
+  },
+  ListBooksView: {
+     screen: ListBooksView,
+     navigationOptions: {
+       header: null
+     }
+   },
+}, {
+  headerMode: 'screen',
+  mode: 'card',
+  initialRouteName: 'ListBooksView'
+})
+
+NavigatorTabBooks.router.getStateForAction = navigateOnce(NavigatorTabBooks.router.getStateForAction);
 
 // =================================================================
 // TAB BAR
@@ -18,16 +53,16 @@ const routeConfiguration = {
     navigationOptions: {
       tabBarLabel: 'Articles',
       tabBarIcon: ({ focused }) => {
-        return focused ? <Icon name="md-paper" size={25} color={Color.GREEN_TOSCA}/> : <Icon name="md-paper" size={25} color={Color.BLACK}/>
+        return focused ? <Icon name="md-paper" size={25} color={Color.DARK_GREEN}/> : <Icon name="md-paper" size={25} color={Color.GREEN_TOSCA}/>
       }
     }
   },
   TabBooksNavigation: {
-    screen: ListBooksView,
+    screen: NavigatorTabBooks,
     navigationOptions: {
       tabBarLabel: 'Books',
       tabBarIcon: ({ focused }) => {
-        return focused ? <Icon name="md-book" size={25} color={Color.GREEN_TOSCA}/> : <Icon name="md-book" size={25} color={Color.BLACK}/>
+        return focused ? <Icon name="md-book" size={25} color={Color.DARK_GREEN}/> : <Icon name="md-book" size={25} color={Color.GREEN_TOSCA}/>
       }
     }
   }
@@ -38,9 +73,9 @@ const tabBarConfiguration = {
   tabBarComponent: TabBarBottom,
   tabBarPosition: 'bottom',
   tabBarOptions: {
-    activeTintColor: Color.GREEN_TOSCA,
-    inactiveTintColor: Color.BLACK,
-    style: {borderTopWidth:1, borderColor:Color.GREEN_TOSCA, backgroundColor:Color.WHITE},
+    activeTintColor: Color.DARK_GREEN,
+    inactiveTintColor: Color.GREEN_TOSCA,
+    tabStyle: {borderTopWidth:1, borderColor:Color.LIGHT_GREEN, backgroundColor:Color.WHITE},
     labelStyle: {fontWeight:'bold', fontSize:12}
   }
 }
