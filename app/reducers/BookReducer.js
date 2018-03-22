fd1f58d733424ee311c7ee5fbbb3a0a3b867399a
Robby Widya Hartono <robby.widyahartono@gmail.com>
2018-03-22 08:23:42 +0700

    add book reducer for redux

---
+++ app/reducers/BookReducer.js
@@ -0,0 +1,30 @@
+import * as ActionTypes from 'kumparan_mobile/app/actions/ActionTypes';
+
+const initialState = {
+  list: 'e-book-fiction',
+  data: []
+}
+
+export const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.GET_BOOKS_LIST_ERROR:
+      return {
+        ...state,
+        isRequest:false
+      }
+    case ActionTypes.GET_BOOKS_LIST_REQUEST:
+      return {
+        ...state,
+        isRequest:true
+      }
+    case ActionTypes.GET_BOOKS_LIST_SUCCESS:
+      return {
+        ...state,
+        isRequest:false,
+        data: action.data,
+        page: action.page
+      }
+    default:
+      return state;
+  }
+}
